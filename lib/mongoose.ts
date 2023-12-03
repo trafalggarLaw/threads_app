2dd767339107650b2fbc6a0e0c1a9db4fbe419aa
Harsh Singh <harshsingh@Harshs-MacBook-Air.local>
2023-12-03 20:01:43 +0530

    first commit

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -7,19 +7,24 @@ export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
 
   if (!process.env.MONGODB_URL) return console.log("Missing MongoDB URL");
+  console.log("just checking!!")
 
   // If the connection is already established, return without creating a new connection.
   if (isConnected) {
     console.log("MongoDB connection already established");
     return;
+  }else{
+    console.log("NOT CONNECTED!!!")
+
   }
 
   try {
     await mongoose.connect(process.env.MONGODB_URL);
 
     isConnected = true; // Set the connection status to true
-    console.log("MongoDB connected");
+    console.log("inside MongoDB ");
   } catch (error) {
+    console.log("inside else")
     console.log(error);
   }
 };
